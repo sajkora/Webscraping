d4e126a4f27455e625d538e35cabdad61b7ddf97
MikCzar <miki.gd2003@gmail.com>
2024-03-31 23:32:28 +0200

    500 Internal Server Error

--- backend/Scraping/main.py
+++ backend/Scraping/main.py
@@ -3,24 +3,35 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from playwright.sync_api import sync_playwright
 from time import sleep
+import random
+import csv
+from io import StringIO
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://www.coingecko.com/en/all-cryptocurrencies")
-    print(page.title())
-    for _ in range (1,6):
-        page.click(selector='[data-target="all-coins.showMore"]')
-        sleep(0.5)
-    kontent = page.content()
-    browser.close()
+def scrape_data():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://www.coingecko.com/en/all-cryptocurrencies")
+        print(page.title())
+        for _ in range (1,6):
+            page.click(selector='[data-target="all-coins.showMore"]')
+            sleep(0.5)
+        kontent = page.content()
+        browser.close()
 
-tables = []
-soup = BeautifulSoup(kontent, 'html.parser')
-df_list = pd.read_html(str(soup))
-for df in df_list:
-    tables.append(df)
+    tables = []
+    soup = BeautifulSoup(kontent, 'html.parser')
+    df_list = pd.read_html(str(soup))
+    for df in df_list:
+        tables.append(df)
 
-master_table = pd.concat(tables)
-master_table = master_table.loc[:,master_table.columns[1:-1]]
-master_table.to_csv('Crypto Data.csv', index=False)
+    master_table = pd.concat(tables)
+    master_table = master_table.loc[:,master_table.columns[1:-1]]
+    master_table.to_csv('Crypto Data.csv', index=False)
+
+def get_random_crypto():
+    with open('Crypto Data.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        rows = list(reader)
+        random_row = random.choice(rows)
+    return random_row
--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, url_for
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
+import logging
+
+logging.basicConfig(stream=sys.stderr)
+
+import main
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/generate', methods=['GET'])
+def generate():
+    print("Calling generate function...")
+    random_crypto = main.get_random_crypto()
+    return render_template('index.html', random_crypto=random_crypto)
 
 if __name__ == '__main__':
     app.run()
